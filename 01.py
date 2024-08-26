c7dfb294ae16d0332f8711c6474f3d84c5ab4c26
Yash <yashtomar138@gmail.com>
2024-08-26 19:38:18 +0530

    01.py modified

--- 01.py
+++ 01.py
@@ -1,27 +1,32 @@
-ls =[]
+''' Q1-> Given an array A [] of n numbers and another number x, the task
+         is to check whether or not there exist two elements in A [] whose
+         sum is exactly x. '''
+
+
+A =[]
 n = int(input("Enter no. of elements u want in a list "))
 for i in range(0,n):
-    x = int(input("enter element "))
-    ls.append(x)
-print(ls)
+    num = int(input("enter element "))
+    A.append(num)
+print(A)
 
 
-num = int(input("Enter number (num)"))
-print(num)
+x = int(input("Enter number (x)"))
+print(x)
 
 
 flag = False
 
-for i in ls:
+for i in A:
     temp = i
     # print(temp)
 
-    ls1=ls.copy()
+    ls1=A.copy()
     ls1.remove(temp)
     # print(ls1)
 
     for j in ls1:
-        req_num = num - temp
+        req_num = x - temp
         if req_num in ls1:
             flag =True
         if flag == True:
